215f04502cf8a7e867926b407de70cce7b43615a
Renzaki <renzakithakuri@gmail.com>
2024-06-09 10:02:17 +0545

    getStaticProps

--- fetching/pages/index.js
+++ fetching/pages/index.js
@@ -1,11 +1,23 @@
-function HomePage(props) {
+import fs from "fs/promises";
+import path from "path";
+function HomePage({ products }) {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+// should always return props , this function gets executed first , run codes that will never be visivle in client side
+export async function getStaticProps() {
+  // to get current working directory with process.cwd() , with this our cwd is the overall project rather than in index.js
+  const filePath = path.join(process.cwd(), "data", "dummy-bakend.json");
+  const jsonData = await fs.readFile(filePath);
+  // Json.parse converts json into regular js object
+  const data = JSON.parse(jsonData);
+  return { props: { products: data.products } };
 }
 
 export default HomePage;
